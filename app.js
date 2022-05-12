332d3bed388e091ae9e8d11faf38f3c5f3c0d313
Anass Gassi <anassgass1997@gmail.com>
2022-05-12 22:28:32 +0100

    Express Session part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
@@ -32,12 +34,22 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //enable cookie
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+
+//setup of the session in the middleware
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 //implement authentification
 function auth(req, res, next){
-  console.log(req.signedCookies);
-  if(!req.signedCookies.user){
+  console.log(req.session);
+  if(!req.session.user){
     var authHeader=req.headers.authorization;
 
     if(!authHeader){
@@ -55,7 +67,7 @@ function auth(req, res, next){
 
 
     if(username==='admin' && password==='password'){
-      res.cookie('user','admin',{ signed: true});
+      req.session.user= 'admin'
       next();
     }
     else{
@@ -67,7 +79,7 @@ function auth(req, res, next){
     }
   }
   else{
-    if(req.signedCookies.user==='admin'){
+    if(req.session.user==='admin'){
       next();
     }
     else{
