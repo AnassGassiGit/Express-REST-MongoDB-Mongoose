f892bbc2f4abf01c2d45d6641f5301f390b9b037
Anass Gassi <anassgass1997@gmail.com>
2022-05-12 19:19:33 +0100

    Basic Authentification

--- models/promotions.js
+++ models/promotions.js
@@ -3,7 +3,6 @@ const Schema=mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency=mongoose.Types.Currency;
 
-
 const promotionSchema = new Schema({
     name: {
         type: String,
